d63b6e071ca59f0573f5d227e7f6224228a8c269
Igor Perkovic <igorp.mailbox@gmail.com>
2022-04-01 00:04:02 +0200

    Richer format part

--- Parsing_sql.py
+++ Parsing_sql.py
@@ -1,11 +1,13 @@
-#!/home/igorp/python_venv/data/bin/python
 # -*- coding: utf8 -*-
 
-import sys
-import sqlparse
+# 📦 PACKAGE:
+# pip install sqlparse
 
+import sys
 if len(sys.argv):
     # Join all arguments into one string
     sql = ' '.join(sys.argv[1:])
-    parsed = sqlparse.format(sql, reindent=True, keyword_case='upper')
+
+    import sqlparse
+    parsed = sqlparse.format(sql, indent_width=4, reindent_aligned=True, use_space_around_operators=True, comma_first=True, keyword_case='upper')
     print(parsed)
